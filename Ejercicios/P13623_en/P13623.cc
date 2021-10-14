7af2a98e6d952cd1ddc28d70a6396d9be96f13db
raulgilabert <raulgilabertgamez@gmail.com>
2021-10-14 11:28:35 +0200

    Renamed vars

--- Ejercicios/P13623_en/P13623.cc
+++ Ejercicios/P13623_en/P13623.cc
@@ -9,14 +9,15 @@ white squares being the top-left one white.
 using namespace std;
 
 int main() {
-    int r, c;
-    cin >> r >> c;
+    // rown and columns are natural numbers
+    int rows, columns;
+    cin >> rows >> columns;
 
     int sum = 0;
 
     // Read the data from the user and add the numbers to sum
-    for (int i = 0; i < r; ++i) {
-        for (int i2 = 0; i2 < c; ++i2) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
             char num;
             cin >> num;
 
@@ -25,7 +26,7 @@ int main() {
             In odd lines the white squares are in odd positions and in even 
             lines the white squares are in even positions
             */
-            if (i2%2 == i%2) sum += num - '0';
+            if (j%2 == i%2) sum += num - '0';
         }
     }
 
