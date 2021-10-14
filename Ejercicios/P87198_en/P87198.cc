c7ec3483b7aecb099f9d1d38e40a6f4129dcea77
raulgilabert <raulgilabertgamez@gmail.com>
2021-10-14 11:29:46 +0200

    Added P87198

---
+++ Ejercicios/P87198_en/P87198.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    while (cin >> num) {
+        for (int i = 0; i < 3*num-2; ++i) {
+            int white, x;
+
+            // First part of the octagon
+            if (i < num - 1) {
+                white = num - 1 - i;
+                x = num + 2*i;
+            }
+
+            // Middle part of the octagon
+            else if (i > num - 2 and i < 2*num - 1) {
+                white = 0;
+                x = 3*num - 2;
+            }
+
+            // Last part of the octagon
+            else {
+                white = -2*num + i + 2;
+                x = 7*num - 2*i - 6;
+            }
+
+            for (int j = 0; j < white; ++j) cout << ' ';
+            for (int j = 0; j < x; ++j) cout << 'X';
+
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
